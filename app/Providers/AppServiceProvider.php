bf20a556b4a3054df91e8c435ccc27bbc0069402
Airlangga Joyonegoro <airlanggaj@gmail.com>
2022-08-23 16:06:56 +0700

    sssssiuuu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
    */
   public function boot()
   {
+    if (env('APP_ENV') !== 'local') {
+      URL::forceScheme('https');
+    }
   }
 }
